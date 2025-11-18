3e11997f7fa711fc6cd4004c29e8046bfbd4ef2c
Emma Gairin <156651973+Emma-Gairin@users.noreply.github.com>
2025-11-18 12:43:35 +0900

    Update import_data.R

--- R/import_data.R
+++ R/import_data.R
@@ -9,9 +9,15 @@ import_data <- function(expr_path, sample_meta_path, gene_meta_path) {
   expr <- read.csv(expr_path, row.names = 1, check.names = FALSE)
   sample_meta <- read.csv(sample_meta_path)
   gene_meta <- read.csv(gene_meta_path)
+  print(paste0(length(intersect(rownames(expr),gene_meta$gene))," genes were retrieved in the row names of the expression matrix."))
+  if(length(intersect(rownames(expr),gene_meta$gene))==0){
+    stop("No object from column gene of the biological information match the rownames of the counts matrix. \n Make sure that that the first column of your expression CSV file corresponds to the gene names/IDs.")
+  }
+  print(paste0(length(intersect(colnames(expr),sample_meta$sample))," samples were retrieved in the column names of the expression matrix."))
 
   if (!all(sample_meta$sample%in% colnames(expr))) {
-    stop("Sample names in metadata do not match expression matrix column names.")
+    warning("Sample names in metadata do not match expression matrix column names. We only kept the ones in common")
+    sample_meta = sample_meta[which(sample_meta$sample%in%colnames(expr)),]
   }
   return(list(expr = expr, sample_meta = sample_meta, gene_meta = gene_meta))
 }
